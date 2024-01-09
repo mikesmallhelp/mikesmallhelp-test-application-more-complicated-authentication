1405375dc59312ac91557c3ea4d11b6f194c802a
Mike <mikesmallhelp@gmail.com>
2024-01-09 16:53:15 +0200

    Add the authentication parts

---
+++ components/FrontPage.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import PageTemplate from './PageTemplate';
+
+const FrontPage = () => {
+    const [loading, setLoading] = useState(true);
+
+    const content = (
+        <div>
+            <a href='/working-page'>Page 1</a>
+            <a href='/working-page2'>Page 2</a>
+        </div>
+    );
+
+    return (
+        <>
+            <PageTemplate title={'Test application'} content={content} 
+                   loading={loading} setLoading={setLoading} showLogoutLink={true}/>
+        </>
+    );
+};
+
+export default FrontPage;
